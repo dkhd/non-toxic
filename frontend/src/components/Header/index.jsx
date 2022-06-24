c617616395da11c670c0619a3a06902ce603bce6
Diky Hadna <hi@hadna.space>
2022-06-24 18:50:17 +0700

    change: about URL

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -6,7 +6,7 @@ function Header() {
       <img src="./non-toxic.png" className="w-14" alt="logo" />
       <div className="grow" />
       <a
-        to="https://note.hadna.space/detect-toxic-comments-in-your-app-using-toxicity-space"
+        href="https://note.hadna.space/detect-toxic-comments-in-your-app-using-toxicity-space"
         className="bg-bermuda-600 text-malibu-100 font-light my-auto p-3 hover:bg-bermuda-200 hover:text-bermuda-600 rounded-md"
       >
         About
